4f8eea68ea790d39dc5b6173adb0aa9e90457ac0
dee-mos <dee-ru@yandex.ru>
2023-10-10 17:28:41 +0300

    other

--- Clock7219/src/main.cpp
+++ Clock7219/src/main.cpp
@@ -11,7 +11,7 @@
 
 #include "constants.h"
 
-#define DEVICE_VERSION "dee-mos/gyverhub_ws2812_clock@1.04"
+#define DEVICE_VERSION "dee-mos/Electronics/Clock7219@1.05"
 
 #define AP_SSID "Rakabas"
 #define AP_PASS "abcdef2908"
